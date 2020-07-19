a7ac127b3a007f7d25523f3e57190f2a1fbddc46
abdullahmhd <shk.mhd.abdullah@gmail.com>
2020-07-19 17:22:50 +0530

    Implement map based service. Closes #6.

--- pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/services/VetService.java
@@ -4,5 +4,4 @@ import guru.sf.sfgpetclinic.model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
 
-
 }
---
+++ pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/services/map/PetSserviceMap.java
@@ -0,0 +1,33 @@
+package guru.sf.sfgpetclinic.services.map;
+
+import guru.sf.sfgpetclinic.model.Pet;
+import guru.sf.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetSserviceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
