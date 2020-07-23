1580c5b4c2e81b00def2d8cdda807dbb8585b69a
abdullahmhd <shk.mhd.abdullah@gmail.com>
2020-07-23 19:46:35 +0530

    Created Spring Data JPA service for Vet. Closes #41.

---
+++ pet-clinic-data/src/main/java/guru/sf/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,49 @@
+package guru.sf.sfgpetclinic.services.springdatajpa;
+
+import guru.sf.sfgpetclinic.model.Vet;
+import guru.sf.sfgpetclinic.repositories.VetRepository;
+import guru.sf.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        this.vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return this.vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return this.vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        this.vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.vetRepository.deleteById(id);
+    }
+}
